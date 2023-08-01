dcbe3d2846e60aacf0e11e77bfe6347e3d594d68
yashkumarbhojyan <135320053+yashkumarbhojyan@users.noreply.github.com>
2023-08-01 10:37:13 +0530

    index.html uploaded

---
+++ script.js
@@ -0,0 +1,63 @@
+// const thumbnail1 = document.getElementById('thumbnail1');
+// const thumbnail2 = document.getElementById('thumbnail2');
+// const thumbnail3 = document.getElementById('thumbnail3');
+// const thumbnail4 = document.getElementById('thumbnail4');
+// const thumbnail5 = document.getElementById('thumbnail5');
+// const thumbnail6 = document.getElementById('thumbnail6');
+// const thumbnail_1 = document.getElementById('thumbnail_1');
+// const thumbnail_2 = document.getElementById('thumbnail_2');
+// const thumbnail_3 = document.getElementById('thumbnail_3');
+// const thumbnail_4 = document.getElementById('thumbnail_4');
+// var videoPlayer = document.getElementById('videoPlayer');
+
+// thumbnail1.addEventListener('click', function() {
+//   // if(id=thumbnail1){
+//     videoPlayer.src = 'images/video.mp4';
+//   videoPlayer.play();
+  
+  
+// });
+// thumbnail2.addEventListener('click', function() {
+// // if(id=thumbnail2){
+//   videoPlayer.src = 'images/riding.mp4';
+//   videoPlayer.play();
+  
+// });
+// thumbnail3.addEventListener('click', function() {
+//   // if(id=thumbnail3){
+//     videoPlayer.src = 'images/technology.mp4';
+//     videoPlayer.play();
+  
+  
+// });
+// thumbnail4.addEventListener('click', function() {
+// // if(id=thumbnail4){
+//   videoPlayer.src = 'images/mountains.mp4';
+//   videoPlayer.play();
+  
+// });
+// thumbnail5.addEventListener('click', function() {
+//   // if(id=thumbnail4){
+//     videoPlayer.src = 'images/friends.mp4';
+//     video.play();
+    
+//   });
+//   thumbnail6.addEventListener('click', function() {
+//     // if(id=thumbnail4){
+//       videoPlayer.src = 'images/cars.mp4';
+//       videoPlayer.play();
+      
+//     });
+
+
+
+
+
+  // // Retrieve the video source URL from the query parameter
+  // const queryParams = new URLSearchParams(window.location.search);
+  // const videoSource = queryParams.get('videoSource');
+
+  // // Set the source of the video element
+  // const videoPlayer = document.getElementById('videoPlayer');
+  // videoPlayer.src = videoSource;
+
